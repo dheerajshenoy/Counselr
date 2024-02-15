f6e823387067104f41dfd7d951cf016802ad7b72
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-02-15 21:49:55 +0530

    see in README.md

--- Client/src/components/HomepageComponents/Carouselcmp.jsx
+++ Client/src/components/HomepageComponents/Carouselcmp.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "react-slideshow-image/dist/styles.css";
 import { Fade, Zoom, Slide } from "react-slideshow-image";
 import third from "../Image/iStock-1072470136.jpg"
-import first from "../Image/360_F_411196492_ALeI8x6briszcGigHOP3rWfkNqDh7D18.jpg"
+import first from "../Image/img1.jpg"
 import second from "../Image/Primary_and_secondary_education_206b_383a1eb6f2.jpg"
 import css from "./CSS/Carouselcmp.css"
 
 let images = [
-    { url: third, text: "Best Counselling Platform"},
-    { url: second, text: "TEXT 2"},
-    { url: first, text: "TEXT 3"},
+    { url: third, text: "Best Counselling Platform" },
+    { url: second, text: "TEXT 2" },
+    { url: first, text: "TEXT 3" },
 ];
 
 let divStyle = {
@@ -17,7 +17,8 @@ let divStyle = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    height: "550PX",
+    width: "auto",
+    height: "550px",
     backgroundSize: "cover",
     zIndex: 1,
 };
